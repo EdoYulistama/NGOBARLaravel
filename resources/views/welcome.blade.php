d4b2d6457bee903d4b855492195ab0d43bc0d3dd
Edo Yulistama <yulistama.yy@gmail.com>
2019-09-16 07:00:35 +0700

    ngobar 17

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    YuListama
                 </div>
 
                 <div class="links">
