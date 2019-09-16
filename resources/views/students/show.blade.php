f20f43fb5973c71908facafe5b42eaeec77d740a
Edo Yulistama <yulistama.yy@gmail.com>
2019-09-16 13:38:43 +0700

    students show

---
+++ resources/views/students/show.blade.php
@@ -0,0 +1,27 @@
+@extends('layout.main')
+
+@section('title', 'Detail Mahasiswa')
+    
+@section('container')
+  <div class="container">
+    <div class="row">
+      <div class="col-6">
+        <h1 class="mt-3">Detail Mahasiswa</h1>
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                  <h5 class="card-title">{{$students->nama}}</h5>
+                  <h6 class="card-subtitle mb-2 text-muted">{{$students->npm}}</h6>
+                <p class="card-text">{{$students->email}}</p>
+                <p class="card-text">{{$students->jurusan}}</p>
+                    
+                    <button type="submit" class="btn btn-primary">Edit</button>
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                  
+                <a href="{{url('/students')}}" class="card-link">Kembali</a>
+                </div>
+              </div>
+      </div>
+    </div>
+  </div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,7 @@
 Route::get('/', 'PagesController@home');
 Route::get('/about', 'PagesController@about');
 Route::get('/mahasiswa', 'MahasiswaController@index');
+
+//student
+Route::get('/students', 'StudentsController@index');
+Route::get('/students/{student}', 'StudentsController@show');
