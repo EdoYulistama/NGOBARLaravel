e9103c3855daea572818990c7eb9363202c03aaa
Edo Yulistama <yulistama.yy@gmail.com>
2019-09-16 10:02:54 +0700

    mahasiswa database

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,7 +6,7 @@
   <div class="container">
     <div class="row">
       <div class="col-10">
-        <h1 class="mt-3">Hello, YuListama!</h1>
+        <h1 class="mt-3">{{$nama}}</h1>
       </div>
     </div>
   </div>
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
-Route::get('/', function () {
-    return view('index');
-});
+// Route::get('/', function () {
+//     return view('index');
+// });
 
-Route::get('/about', function () {
-    return view('about');
-});
+// Route::get('/about', function () {
+//     return view('about');
+// });
+
+Route::get('/', 'PagesController@home');
+Route::get('/about', 'PagesController@about');
+Route::get('/mahasiswa', 'MahasiswaController@index');
